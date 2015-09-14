5179749c8c3c4ade2309494748c891091ab4fec0
张鹏 <scbeta@qq.com>
2015-09-14 10:59:00 +0800

    在Linux下运行时将Console.ReadLine()修改为Thread.CurrentThread.Join以兼容后台运行。

--- Source/ServerManage/Program.cs
+++ Source/ServerManage/Program.cs
@@ -1,15 +1,20 @@
 ﻿using ServerManage.Static;
 using System;
+using System.Threading;
 
 namespace ServerManage
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             AssemblyAutoSearcher.Init();
             Startup.Start();
-            Console.ReadLine();
+            if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+                Console.ReadLine();
+            else
+                Thread.CurrentThread.Join();
         }
     }
 }
