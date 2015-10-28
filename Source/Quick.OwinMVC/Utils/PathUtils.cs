926eb3426299d0347b11b8a2561e44ae25e282b6
张鹏 <scbeta@qq.com>
2015-10-28 15:16:28 +0800

    让properties文件中的include指令支持通配符

---
+++ Source/Quick.OwinMVC/Utils/PathUtils.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Quick.OwinMVC.Utils
+{
+    public class PathUtils
+    {
+        private static bool hasSearchChar(String searchPath)
+        {
+            return searchPath.Contains("*") | searchPath.Contains("?");
+        }
+
+        /// <summary>
+        /// 搜索文件
+        /// </summary>
+        /// <param name="searchPath"></param>
+        /// <returns></returns>
+        public static IEnumerable<String> SearchFile(String searchPath)
+        {
+            if (hasSearchChar(searchPath))
+            {
+                var searchFileName = Path.GetFileName(searchPath);
+                var searchBaseFolder = Path.GetDirectoryName(searchPath);
+                var baseFolders = SearchFolder(searchBaseFolder);
+                List<String> list = new List<string>();
+                foreach (var baseFolder in baseFolders)
+                {
+                    list.AddRange(Directory.EnumerateFiles(baseFolder, searchFileName));
+                }
+                return list;
+            }
+            else
+            {
+                FileInfo file = new FileInfo(searchPath);
+                if (file.Exists)
+                    return new String[] { file.FullName };
+            }
+            return new String[0];
+        }
+
+        /// <summary>
+        /// 搜索目录
+        /// </summary>
+        /// <param name="searchPath"></param>
+        /// <returns></returns>
+        public static IEnumerable<String> SearchFolder(String searchPath)
+        {
+            if (String.IsNullOrEmpty(searchPath))
+                return new String[] { Environment.CurrentDirectory };
+
+            if (hasSearchChar(searchPath))
+            {
+                var baseFolder = Path.GetDirectoryName(searchPath);
+                var folderName = searchPath.Substring(baseFolder.Length);
+                while (folderName.StartsWith(Path.DirectorySeparatorChar.ToString()))
+                    folderName = folderName.Substring(1);
+                IEnumerable<String> parentFolders = SearchFolder(baseFolder);
+                List<String> list = new List<string>();
+                foreach (var parentFolder in parentFolders)
+                    list.AddRange(Directory.EnumerateDirectories(parentFolder, folderName));
+                return list;
+            }
+            else
+            {
+                DirectoryInfo folder = new DirectoryInfo(searchPath);
+                if (folder.Exists)
+                    return new String[] { folder.FullName };
+            }
+            return new String[0];
+        }
+    }
+}
--- Source/Quick.OwinMVC/Utils/PropertyUtils.cs
+++ Source/Quick.OwinMVC/Utils/PropertyUtils.cs
@@ -10,10 +10,10 @@ namespace Quick.OwinMVC.Utils
 {
     public class PropertyUtils
     {
-        public static IDictionary<String, String> Load(String content, String folderPath)
+        public static IDictionary<String, String> Load(String content, String folderPath, params String[] notAllowFiles)
         {
             IDictionary<String, String> dict = new Dictionary<String, String>();
-            content = parse(content, folderPath);
+            content = parse(content, folderPath, new HashSet<string>(notAllowFiles));
             //参考正则表达式: ^\s*(?'key'[^#][^\s]*?)\s*=\s*(?'value'.*)\s*$
             Regex regex = new Regex(@"^\s*(?'key'[^#][^\s]*?)\s*=\s*(?'value'.*)\s*$", RegexOptions.Multiline);
             foreach (Match match in regex.Matches(content))
@@ -26,7 +26,7 @@ namespace Quick.OwinMVC.Utils
             return dict;
         }
 
-        private static String parse(String content, String folderPath)
+        private static String parse(String content, String folderPath, HashSet<String> notAllowFiles)
         {
             content = content.Replace("\\\r\n", "").Replace("\\\r", "").Replace("\\\n", "").Replace("\r", "");
 
@@ -36,10 +36,22 @@ namespace Quick.OwinMVC.Utils
             content = regex.Replace(content, match =>
              {
                  var fileName = match.Groups["fileName"].Value;
-                 if (!File.Exists(fileName))
+                 List<String> list = new List<string>();
+                 list.AddRange(PathUtils.SearchFile(fileName));
+                 if (list.Count == 0)
+                 {
                      fileName = Path.Combine(folderPath, fileName);
-                 FileInfo file = new FileInfo(fileName);
-                 return parse(File.ReadAllText(file.FullName), file.DirectoryName);
+                     list.AddRange(PathUtils.SearchFile(fileName));
+                 }
+                 StringBuilder sb = new StringBuilder();
+                 foreach (var currentFileName in list)
+                 {
+                     FileInfo file = new FileInfo(currentFileName);
+                     if (notAllowFiles.Contains(file.FullName))
+                         continue;
+                     sb.AppendLine(parse(File.ReadAllText(file.FullName), file.DirectoryName, notAllowFiles));
+                 }
+                 return sb.ToString();
              });
             return content;
         }
@@ -47,7 +59,7 @@ namespace Quick.OwinMVC.Utils
         public static IDictionary<String, String> LoadFile(String fileName)
         {
             FileInfo file = new FileInfo(fileName);
-            return Load(File.ReadAllText(file.FullName), file.DirectoryName);
+            return Load(File.ReadAllText(file.FullName), file.DirectoryName, file.FullName);
         }
     }
 }
