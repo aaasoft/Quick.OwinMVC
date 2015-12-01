ff80d73ef609b2be9f00ccacc6c7640a1f89700f
张鹏 <scbeta@qq.com>
2015-12-01 12:52:16 +0800

    完善主页图表的显示效果。

--- Source/Plugins/SvnManage/resource/js/includeMasterPage.js
+++ Source/Plugins/SvnManage/resource/js/includeMasterPage.js
@@ -8,17 +8,29 @@ loadJavascript("../../base/resource/js/jquery-1.10.2.min.js");
 loadJavascript("../../base/resource/js/template.js");
 
 window.onload = function (e) {
+    //移除footer元素
     $("footer").remove();
+    //取出meta中的数据
     var masterPage = $("meta[name='masterPage']").attr('content');
     var title = $("meta[name='title']").attr('content');
+    var scripts = $("meta[name='scripts']").attr('content');
+    //移除meta元素
+    $("meta").remove();
+
     $.get(masterPage, function (source) {
         template.config("escape", false);
 
+        var headHTML = $("head")[0].innerHTML;
+        if (scripts) {
+            scripts = scripts.split(';');
+            for (var i in scripts)
+                headHTML = '<script src="' + scripts[i] + '"></script>' + headHTML;
+        }
         var render = template.compile(source);
         var html = render({
             title: title,
             body: $("body")[0].innerHTML,
-            script: $("head")[0].innerHTML
+            script: headHTML
         });
         document.removeChild(document.all[0]);
         document.write(html);
