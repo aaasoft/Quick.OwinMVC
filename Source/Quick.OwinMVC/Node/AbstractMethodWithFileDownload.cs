0a5a6bd860bc3cf64a673459791c1cf48d3c8f9b
张鹏 <scbeta@qq.com>
2016-08-23 14:24:08 +0800

    增加文件下载与流输出的抽象基类。

---
+++ Source/Quick.OwinMVC/Node/AbstractMethodWithFileDownload.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using System.IO;
+using Quick.OwinMVC.Utils;
+
+namespace Quick.OwinMVC.Node
+{
+    public class FileDownloadResult : StreamOutputResult
+    {
+        /// <summary>
+        /// 文件名称(附件文件名)
+        /// </summary>
+        public string FileName { get; set; }
+    }
+
+    public abstract class AbstractMethodWithFileDownload<TInput> : AbstractMethod<TInput>
+                where TInput : class
+    {
+        public abstract FileDownloadResult HandleDownload(IOwinContext context, TInput input);
+
+        public sealed override object Invoke(IOwinContext context, TInput input)
+        {
+            var result = HandleDownload(context, input);
+
+            var rep = context.Response;
+            //输出附件文件名
+            if (!string.IsNullOrEmpty(result.FileName))
+                rep.Headers["Content-Disposition"] = $"attachment; filename=\"{result.FileName}\"; filename*=UTF-8''{result.FileName}";
+            //内容大小
+            rep.ContentLength = result.ContentLength;
+            //内容类型
+            if (string.IsNullOrEmpty(result.ContentType)
+                && !string.IsNullOrEmpty(result.FileName))
+                rep.ContentType = MimeUtils.GetMime(result.FileName);
+            else
+                rep.ContentType = result.ContentType;
+            //操作流
+            result.ResponseStreamHandler.Invoke(rep.Body);
+            throw NodeMethodHandledException.Instance;
+        }
+    }
+}
---
+++ Source/Quick.OwinMVC/Node/AbstractMethodWithStreamOutput.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Owin;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quick.OwinMVC.Node
+{
+    public class StreamOutputResult
+    {
+        /// <summary>
+        /// 文件大小
+        /// </summary>
+        public long? ContentLength { get; set; }
+        /// <summary>
+        /// 文件类型
+        /// </summary>
+        public string ContentType { get; set; }
+        /// <summary>
+        /// 响应流处理器
+        /// </summary>
+        public Action<Stream> ResponseStreamHandler { get; set; }
+    }
+
+    public abstract class AbstractMethodWithStreamOutput<TInput> : AbstractMethod<TInput>
+                where TInput : class
+    {
+        public abstract StreamOutputResult HandleOutput(IOwinContext context, TInput input);
+
+        public sealed override object Invoke(IOwinContext context, TInput input)
+        {
+            var result = HandleOutput(context, input);
+            var rep = context.Response;
+            //内容大小
+            rep.ContentLength = result.ContentLength;
+            //内容类型
+            if (!string.IsNullOrEmpty(result.ContentType))
+                rep.ContentType = result.ContentType;
+            result.ResponseStreamHandler.Invoke(rep.Body);
+            throw NodeMethodHandledException.Instance;
+        }
+    }
+}
