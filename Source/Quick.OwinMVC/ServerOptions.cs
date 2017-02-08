7617601fb83e0802e2106cb5cfb609c93bcbb2b4
张鹏 <scbeta@qq.com>
2017-02-08 11:27:47 +0800

    使用Microsoft的Owin容器时，检测是否使用管理员权限运行，如果不是，则使用127.0.0.1。

---
+++ Source/Quick.OwinMVC/ServerOptions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quick.OwinMVC
+{
+    [Serializable]
+    public class ServerOptions
+    {
+        public string Name { get; set; }
+        public IPEndPoint Endpoint { get; set; }
+        public string Wrapper { get; set; }
+        public string DefaultLanguage { get; set; } = "zh-CN";
+        public string Middlewares { get; set; }
+
+        public IDictionary<string, string> Properties { get; set; }
+
+        public ServerOptions(
+            string name,
+            IPEndPoint endpoint,
+            string wrapper,
+            string middlewares,
+            IDictionary<string, string> properties)
+        {
+            this.Name = name;
+            this.Endpoint = endpoint;
+            this.Wrapper = wrapper;
+            this.Middlewares = middlewares;
+            this.Properties = properties;
+        }
+
+        public String GetUrl()
+        {
+            var protocol = "http";
+            var defaultPort = 80;
+
+            string url = $"{protocol}://{Endpoint.Address.ToString()}";
+            if (Endpoint.Port != defaultPort)
+                url += $":{Endpoint.Port}";
+            return url;
+        }
+    }
+}
